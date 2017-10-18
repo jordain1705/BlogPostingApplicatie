4f27b14ee0df9c1a3a597d0f1bf10d641baa5411
Jordain Gijsbertha <jordain1997@hotmail.com>
2017-10-18 16:12:21 +0200

    Show  A post Functionaliteit

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,42 @@
- import React, {Component} from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import {fetchPost} from '../actions';
+import {Link} from 'react-router-dom';
 
 class PostsShow extends Component {
+
+  // as soon as the component shows op on the screen. ComponentDidMount
+  componentDidMount() {
+    const {id} = this.props.match.params; // takes the wild card
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const {post} = this.props;
+
+    if (!post) {
+      return <div>Loading....</div>;
+    }
+
     return (
       <div>
-        Post Show
+        <Link className= "btn btn-warning"to= "/"> Back TO Index </Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+// now we need to take that post that's fetched out of our application level
+// state and put it into our component wit mapstatetoProps()
+function mapStateToProps({
+  posts
+}, ownProps) {
+  return {
+    post: posts[ownProps.match.params.id]
+  }
+}; // now instead of returnig the big list of post we return the post we care about
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,26 @@
 import _ from 'lodash'
-import {FETCH_POSTS} from '../actions'
+import {FETCH_POSTS } from '../actions';
+import {FETCH_POST } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
       // when the FETCH_POSTS action is invoced change the array to and object
+    case FETCH_POST:
+
+    // const post = action.payload.data;
+    // const newState = {...state};
+    // newState[post.id] = post;
+    // return newState
+    //
+    return {...state, [action.payload.data.id]: action.payload.data};
+    //..state = takes all the posts that we have fetched. and put them in the new object we are about to return
+
+
+
+
+
+
+
     case FETCH_POSTS:
 
       return _.mapKeys(action.payload.data, 'id');
